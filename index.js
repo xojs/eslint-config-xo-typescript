dc1b330f4957f57fc250f128f41af8dd16352fee
Sindre Sorhus <sindresorhus@gmail.com>
2019-11-26 11:57:11 +0700

    Add `@typescript-eslint/prefer-optional-chain` rule
    https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-optional-chain.md

--- index.js
+++ index.js
@@ -277,6 +277,7 @@ module.exports = {
 		'@typescript-eslint/prefer-includes': 'error',
 		'@typescript-eslint/prefer-namespace-keyword': 'error',
 		'@typescript-eslint/prefer-nullish-coalescing': 'error',
+		'@typescript-eslint/prefer-optional-chain': 'error',
 		'@typescript-eslint/prefer-readonly': 'error',
 		'@typescript-eslint/prefer-string-starts-ends-with': 'error',
 		'@typescript-eslint/promise-function-async': [
