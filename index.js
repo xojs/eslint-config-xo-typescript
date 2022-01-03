8b40873431586ad5f39c81d2d38836320baf07d1
Younho Choo <yo+github@younho9.com>
2022-01-04 01:42:02 +0900

    Extract get `@typescript-eslint/naming-convention` rule function

--- index.js
+++ index.js
@@ -1,4 +1,78 @@
 'use strict';
+const getNamingConventionRule = ({isTsx}) => ({
+	"@typescript-eslint/naming-convention": [
+	'error',
+	{
+		// selector: ['variableLike', 'memberLike', 'property', 'method'],
+		// Note: Leaving out `parameter` and `typeProperty` because of the mentioned known issues.
+		// Note: We are intentionally leaving out `enumMember` as it's usually pascal-case or upper-snake-case.
+		selector: ['variable', 'function', 'classProperty', 'objectLiteralProperty', 'parameterProperty', 'classMethod', 'objectLiteralMethod', 'typeMethod', 'accessor'],
+		format: [
+			'strictCamelCase',
+			isTsx && 'StrictPascalCase',
+		].filter(Boolean),
+		// We allow double underscope because of GraphQL type names and some React names.
+		leadingUnderscore: 'allowSingleOrDouble',
+		trailingUnderscore: 'allow',
+		// Ignore `{'Retry-After': retryAfter}` type properties.
+		filter: {
+			regex: '[- ]',
+			match: false
+		}
+	},
+	{
+		selector: 'typeLike',
+		format: [
+			'StrictPascalCase'
+		]
+	},
+	{
+		selector: 'variable',
+		types: [
+			'boolean'
+		],
+		format: [
+			'StrictPascalCase'
+		],
+		prefix: [
+			'is',
+			'has',
+			'can',
+			'should',
+			'will',
+			'did'
+		]
+	},
+	{
+		// Interface name should not be prefixed with `I`.
+		selector: 'interface',
+		filter: /^(?!I)[A-Z]/.source,
+		format: [
+			'StrictPascalCase'
+		]
+	},
+	{
+		// Type parameter name should either be `T` or a descriptive name.
+		selector: 'typeParameter',
+		filter: /^T$|^[A-Z][a-zA-Z]+$/.source,
+		format: [
+			'StrictPascalCase'
+		]
+	},
+	// Allow these in non-camel-case when quoted.
+	{
+		selector: [
+			'classProperty',
+			'objectLiteralProperty'
+		],
+		format: null,
+		modifiers: [
+			'requiresQuotes'
+		]
+	}
+	]
+})
+
 module.exports = {
 	parser: require.resolve('@typescript-eslint/parser'),
 	parserOptions: {
@@ -274,76 +348,7 @@ module.exports = {
 		// - https://github.com/typescript-eslint/typescript-eslint/issues/1485
 		// - https://github.com/typescript-eslint/typescript-eslint/issues/1484
 		// TODO: Prevent `_` prefix on private fields when TypeScript 3.8 is out.
-		'@typescript-eslint/naming-convention': [
-			'error',
-			{
-				// selector: ['variableLike', 'memberLike', 'property', 'method'],
-				// Note: Leaving out `parameter` and `typeProperty` because of the mentioned known issues.
-				// Note: We are intentionally leaving out `enumMember` as it's usually pascal-case or upper-snake-case.
-				selector: ['variable', 'function', 'classProperty', 'objectLiteralProperty', 'parameterProperty', 'classMethod', 'objectLiteralMethod', 'typeMethod', 'accessor'],
-				format: [
-					'strictCamelCase'
-				],
-				// We allow double underscope because of GraphQL type names and some React names.
-				leadingUnderscore: 'allowSingleOrDouble',
-				trailingUnderscore: 'allow',
-				// Ignore `{'Retry-After': retryAfter}` type properties.
-				filter: {
-					regex: '[- ]',
-					match: false
-				}
-			},
-			{
-				selector: 'typeLike',
-				format: [
-					'StrictPascalCase'
-				]
-			},
-			{
-				selector: 'variable',
-				types: [
-					'boolean'
-				],
-				format: [
-					'StrictPascalCase'
-				],
-				prefix: [
-					'is',
-					'has',
-					'can',
-					'should',
-					'will',
-					'did'
-				]
-			},
-			{
-				// Interface name should not be prefixed with `I`.
-				selector: 'interface',
-				filter: /^(?!I)[A-Z]/.source,
-				format: [
-					'StrictPascalCase'
-				]
-			},
-			{
-				// Type parameter name should either be `T` or a descriptive name.
-				selector: 'typeParameter',
-				filter: /^T$|^[A-Z][a-zA-Z]+$/.source,
-				format: [
-					'StrictPascalCase'
-				]
-			},
-			// Allow these in non-camel-case when quoted.
-			{
-				selector: [
-					'classProperty',
-					'objectLiteralProperty'
-				],
-				format: null,
-				modifiers: [
-					'requiresQuotes'
-				]
-			}
-		],
+		...getNamingConventionRule({isTsx: false}),
 		'@typescript-eslint/no-base-to-string': 'error',
 		'no-array-constructor': 'off',
 		'@typescript-eslint/no-array-constructor': 'error',
@@ -680,78 +685,7 @@ module.exports = {
 				'**/*.tsx'
 			],
 			rules: {
-				// NOTE: This is a copy of the above rule, and only allows StrictPascalCase.
-				'@typescript-eslint/naming-convention': [
-					'error',
-					{
-						// selector: ['variableLike', 'memberLike', 'property', 'method'],
-						// Note: Leaving out `parameter` and `typeProperty` because of the mentioned known issues.
-						// Note: We are intentionally leaving out `enumMember` as it's usually pascal-case or upper-snake-case.
-						selector: ['variable', 'function', 'classProperty', 'objectLiteralProperty', 'parameterProperty', 'classMethod', 'objectLiteralMethod', 'typeMethod', 'accessor'],
-						format: [
-							'strictCamelCase',
-							'StrictPascalCase'
-						],
-						// We allow double underscope because of GraphQL type names and some React names.
-						leadingUnderscore: 'allowSingleOrDouble',
-						trailingUnderscore: 'allow',
-						// Ignore `{'Retry-After': retryAfter}` type properties.
-						filter: {
-							regex: '[- ]',
-							match: false
-						}
-					},
-					{
-						selector: 'typeLike',
-						format: [
-							'StrictPascalCase'
-						]
-					},
-					{
-						selector: 'variable',
-						types: [
-							'boolean'
-						],
-						format: [
-							'StrictPascalCase'
-						],
-						prefix: [
-							'is',
-							'has',
-							'can',
-							'should',
-							'will',
-							'did'
-						]
-					},
-					{
-						// Interface name should not be prefixed with `I`.
-						selector: 'interface',
-						filter: /^(?!I)[A-Z]/.source,
-						format: [
-							'StrictPascalCase'
-						]
-					},
-					{
-						// Type parameter name should either be `T` or a descriptive name.
-						selector: 'typeParameter',
-						filter: /^T$|^[A-Z][a-zA-Z]+$/.source,
-						format: [
-							'StrictPascalCase'
-						]
-					},
-					// Allow these in non-camel-case when quoted.
-					{
-						selector: [
-							'classProperty',
-							'objectLiteralProperty'
-						],
-						format: null,
-						modifiers: [
-							'requiresQuotes'
-						]
-					}
-				],
+				...getNamingConventionRule({isTsx: true})
 			}
 		}
 	]
