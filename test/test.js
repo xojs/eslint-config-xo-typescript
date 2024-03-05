94e23781f473516e142f19b063d602c834ad5a8a
Mehdy Dara <mehdy.dara.fr@gmail.com>
2024-03-05 14:33:11 +0100

    Fix test and bump AVA

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
-import test from 'ava';
-import {ESLint} from 'eslint';
-import config from '../index.js';
+const test = require('ava');
+const {ESLint} = require('eslint');
+
+const config = require('../index.js');
 
 const hasRule = (errors, ruleId) => errors.some(error => error.ruleId === ruleId);
 
@@ -10,12 +11,13 @@ async function runEslint(string, config) {
 		overrideConfig: config,
 	});
 
-	const [firstResult] = await eslint.lintText(string, {filePath: '_x.ts'});
+	const [firstResult] = await eslint.lintText(string, {filePath: 'test/_x.ts'});
 
 	return firstResult.messages;
 }
 
-test.failing('main', async t => {
+test('main', async t => {
 	const errors = await runEslint('const foo: number = 5;', config);
 	t.true(hasRule(errors, '@typescript-eslint/no-inferrable-types'), JSON.stringify(errors));
+	t.is(errors.length, 1);
 });
